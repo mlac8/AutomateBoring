21119eb4b81f5419b4734ae66d37cf5fad581a8b
Mark Lacanilao <mlac8@outlook.com>
2026-02-05 13:48:08 -0500

    added more basics

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import csv
+import pandas as pd
 
 # THIS IS BASIC SYNTAX EVERYONE SHOULD KNOW FOR AUTOMATED SCRIPTS!!!
 
@@ -9,4 +11,31 @@ with open('file.json', 'r') as f:
 # WRITE JSON FIKLE
 with open('output.json', 'w') as f:
     json.dump(data, f, indent=2)
+
+# access nested data
+value = data['key']['nested_key']
+
+# safe access with default
+value = data.get('key', 'default')
+
+# ITERATE THROUGH LIST OF DICTS
+for item in data:
+    print(item['field'])
+
+#FILE OPERATIONS
+with open('file.txt', 'r') as f:
+    context = f.read()
+
+#READ CSV
+with open('file.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        print(row['column_name'])
+
+#read with pandas
+df = pd.read_csv('file.csv')
+
+
+###############################################
+
 
