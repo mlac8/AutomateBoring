316ea75bf0c5e54e73a7d2b88940bd86422ec389
Mark Lacanilao <mlac8@outlook.com>
2026-02-08 16:25:34 -0500

    doing some more drills. middle of returning users older than 30

--- main.py
+++ main.py
@@ -58,20 +58,57 @@ len(item["numbers"])
 ###########
 
 # DRILL TIME
-
+### return a list of usernames where sum > 40:
 with open('file.json', 'w') as f:
     data = json.load(f)
     for item in data:
         sum(item["numbers"])
  ### WRONG what we want to see is
 
- with open('file.json', 'r') as f:
-     data = json.load(f)
 
-result = []
+
+with open('file.json', 'r') as f:
+    data = json.load(f)
+
+result = [] #create empty list
+
 for item in data:
     total = sum(item["scores"])
     if total > 40:
         result.append(item["user"]["name"])
 
 print(result)
+
+### return a DICTIONARY mapping username to their avg score
+with open('file.json', 'r') as f:
+    data = json.load(f)
+
+mean = []
+
+# iterates through data and scrapes for mean
+
+for item in data:
+    mean = mean.append(item["scores"])
+
+print(mean)
+# THIS IS ALSO INCORRECT. THE CORRECT ANSWER IS:
+
+with open('file.json', 'r') as f:
+    data = json.load(f)
+
+result = {} # THIS IS A DICT. YOU DON'T WANT A [] list for this question
+
+for item in data:
+    name = item["user"]["name"]
+    scores = item["scores"]
+    avg = sum(scores) / len(scores)
+    result[name] = avg
+
+print(result)
+# remember: 1) EXTRACT NAME, 2) EXTRACT LIST, 3) COMPUTE, 4) PUT INTO RESULT
+
+### Return a list of names of users OLDER THAN 30 whose highest score is > 25
+with open('file.json', 'r') as f:
+    data = json.load(f)
+
+result = []
